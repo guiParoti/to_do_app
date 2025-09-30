364837179ee6996be16ad0836f0e1138bd8232e8
Guilherme <guilhermesouza67333@gmail.com>
2025-09-29 21:11:34 -0300

    mais funções no back

--- to_do_back/app.py
+++ to_do_back/app.py
@@ -8,6 +8,20 @@ app = Flask(__name__)
 def pegar_tarefas():
     return jsonify({'tarefas': database.pegar_todas_tarefas()}), 200
 
+@app.route('/tarefas/<tarefa>', methods=['GET'])
+def pegar_uma_tarefa(tarefa):
+    return jsonify({'tarefas': database.pegar_tarefa(tarefa)}), 200
+
+@app.route('/tarefas/<tarefa>', methods=['PUT'])
+def atualizar_tarefa(tarefa):
+    data = request.get_json()
+    tarefa_atualizada = database.atualizar_tarefa(tarefa, data)
+
+    if not atualizar_tarefa:
+        return jsonify({'erro': 'tarefa não encontrada'}), 404
+    
+    return jsonify({'mensagem': 'tarefa atualizada com sucesso', 'tarefa': atualizar_tarefa}), 200
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- to_do_back/database.py
+++ to_do_back/database.py
@@ -6,3 +6,20 @@ colecao_tarefas = db['tarefas']
 
 def pegar_todas_tarefas():
     return list(colecao_tarefas.find({}, {'_id': 0}))
+
+def pegar_tarefa(tarefa):
+    return colecao_tarefas.find_one({'tarefa': tarefa}, {'_id': 0})
+
+def adicionar_nova_tarefa(tarefa):
+    colecao_tarefas.insert_one(tarefa)
+
+def atualizar_tarefa(tarefa, data):
+    resultado = colecao_tarefas.update_one({'tarefa': tarefa}, {'$set': {data}})
+    if resultado.matched_count:
+        return pegar_tarefa(tarefa)
+    return None
+
+def deletar_tarefa(tarefa):
+    resultado = colecao_tarefas.delete_one({'tarefa': tarefa})
+    return resultado.deleted_count > 0
+    
