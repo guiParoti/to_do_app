d763c078c39abe32becd4d73a2c1ceb8153ed932
Guilherme Paroti <guilhermesouza67333@gmail.com>
2025-09-30 23:13:23 -0300

    criei no projeto do vue um componente TelaHome, estou começando o front

--- to_do_front/to_do_app/src/App.vue
+++ to_do_front/to_do_app/src/App.vue
@@ -1,19 +1,12 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import TelaHome from './components/TelaHome.vue';
 import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
-    <TheWelcome />
+    <TelaHome />
   </main>
 </template>
 
---
+++ to_do_front/to_do_app/src/components/TelaHome.vue
@@ -0,0 +1,44 @@
+<script>
+
+
+
+
+
+</script>
+
+<template>
+    <div id="app">
+        <div>
+            <form>
+                <label>Insira o titulo da tarefa:</label>
+                <input type="text" id="tarefa" required="true"></input>
+                <br>
+                <label>Insira a descrição da Tarefa:</label>
+                <input type="text" id="descTarefa" placeholder="Opcional"></input>
+                <br>
+                <button id="btn" type="submit">Salvar tarefa</button>
+            </form>
+
+        </div>
+
+
+
+
+    </div>
+</template>
+
+<style scoped>
+
+#app {
+    flex: auto;
+    flex-direction: column;
+}
+
+#btn {
+    margin-top: 10px;
+
+}
+
+
+
+</style>
